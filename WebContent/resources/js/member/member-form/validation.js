21737e0909c44394bc5f6f0631a1dd158d00f3ee
pgd <rudeh1253@gmail.com>
2024-12-31 19:32:56 +0900

    feat: validation.js

--- WebContent/resources/js/member/member-form/validation.js
+++ WebContent/resources/js/member/member-form/validation.js
@@ -1,6 +1,11 @@
-function validateEmail() {
-    const emailAccount = $("#input[name='email-count']").val();
-    const emailHost = $("#input[name='email-host']").val();
+function validateEmailAccount() {
+    const emailAccount = $("input[name='email-count']").val();
+    
+    return /^[0-9a-z]+$/.test(emailAccount);
+}
 
-    if (/^[0-9a-z]+$/)
+function validateEmailHost() {
+    const emailHost = $("input[name='email-host']").val();
+
+    return /^\w+\.\w(\.\w)?$/.test(emailHost);
 }
