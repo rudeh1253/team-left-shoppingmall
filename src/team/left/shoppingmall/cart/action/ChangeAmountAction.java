64bb0942ba66aa15619b3e761728368215ab5d05
parkjh0927 <dbapjh@gmail.com>
2024-12-31 19:13:28 +0900

    기능 구현 완료

---
+++ src/team/left/shoppingmall/cart/action/ChangeAmountAction.java
@@ -0,0 +1,39 @@
+package team.left.shoppingmall.cart.action;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import team.left.framework.web.CommandHandler;
+import team.left.shoppingmall.cart.dao.CartDao;
+import team.left.shoppingmall.cart.dao.CartProductDto;
+
+public class ChangeAmountAction implements CommandHandler{
+
+	CartDao dao = new CartDao();
+	
+	@Override
+	public String handleCommand(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String command = request.getParameter("command");
+		String productId = request.getParameter("productid");
+		int updatedAmount = 0;
+		System.out.println(command);
+		if("increase".equals(command)) {
+			updatedAmount = dao.increaseCartAmount(Integer.parseInt(productId));
+		}else if("decrease".equals(command)) {
+			updatedAmount = dao.decreaseCartAmount(Integer.parseInt(productId));
+		}else if ("delete".equals(command)) {
+			System.out.println(productId);
+            boolean success = dao.deleteProductFromCart(Integer.parseInt(productId));
+		}
+		
+		System.out.println("success");
+		return "cart/cart-list";
+	}
+
+}
