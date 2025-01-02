9d3009b08aa9690693b62f3f4f0af0f43ea3a6f3
leesumin7766 <hojun7766@naver.com>
2025-01-02 09:08:12 +0900

    0102

--- src/team/left/shoppingmall/purchase/dao/PurchaseDao.java
+++ src/team/left/shoppingmall/purchase/dao/PurchaseDao.java
@@ -90,9 +90,11 @@ public class PurchaseDao {
 			pstmt.setInt(1, userid);
 			
 			ResultSet result = pstmt.executeQuery();
+			
 			while(result.next()) {
 				ReceiptDto dto = new ReceiptDto(
 					result.getString("thumbnail"),
+					
 					result.getString("product_name"),
 					result.getInt("price"),
 					result.getInt("amount"),
@@ -100,7 +102,9 @@ public class PurchaseDao {
 					convertState(result.getString("state"))
 				);
 				list.add(dto);
-			}	
+				System.out.println(dto.toString());
+			}
+			System.out.println(list);
 		}catch(SQLException e) {
 			throw new RuntimeException(e);
 		}finally {
