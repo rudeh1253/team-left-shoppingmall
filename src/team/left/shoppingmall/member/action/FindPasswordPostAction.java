2005585f938a91b2f4de376bc1aabddd759b9d3d
pgd <rudeh1253@gmail.com>
2025-01-02 17:36:38 +0900

    fix: 컴파일 에러

--- src/team/left/shoppingmall/member/action/FindPasswordPostAction.java
+++ src/team/left/shoppingmall/member/action/FindPasswordPostAction.java
@@ -1,15 +1,10 @@
 package team.left.shoppingmall.member.action;
 
 import java.io.IOException;
-import java.sql.SQLException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import team.left.framework.web.CommandHandler;
-import team.left.shoppingmall.member.dao.FindPasswordDao;
 import team.left.shoppingmall.member.dao.MemberDao;
 
 public class FindPasswordPostAction implements CommandHandler {
