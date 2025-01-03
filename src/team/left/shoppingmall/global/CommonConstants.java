cb1d5b5e274f1c0829b494a4393e842a1f50e49b
pgd <rudeh1253@gmail.com>
2025-01-03 15:46:20 +0900

    feat: 세션에 role 추가

--- src/team/left/shoppingmall/global/CommonConstants.java
+++ src/team/left/shoppingmall/global/CommonConstants.java
@@ -5,4 +5,5 @@ public class CommonConstants {
     public static final int BUFFER_SIZE = 1024;
     public static final String MEMBER_NAME_SESSION_KEY = "memberName";
     public static final String MEMBER_PROFILE_IMG_SESSION_KEY = "memberProfileImg";
+    public static final String MEMBER_ROLE_SESSION_KEY = "role";
 }
