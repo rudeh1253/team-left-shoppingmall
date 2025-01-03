afe66bdac80e63a9b9c784d21825f6ff85e3f075
pgd <rudeh1253@gmail.com>
2025-01-03 16:47:40 +0900

    feat: 인증/인가 적용

--- src/team/left/shoppingmall/global/AuthenticationFilter.java
+++ src/team/left/shoppingmall/global/AuthenticationFilter.java
@@ -1,11 +1,15 @@
 package team.left.shoppingmall.global;
 
 import java.io.IOException;
-import java.util.HashSet;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -13,15 +17,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AuthenticationFilter implements Filter {
-    private static final Set<CommandRequestInfo> BLACK_LIST;
-    private static final String GET = "GET";
-    private static final String POST = "POST";
+    private Set<CommandRequestInfo> blackList = null;
     
-    static {
-        Set<CommandRequestInfo> blackList = new HashSet<>();
-        blackList.add(new CommandRequestInfo("/member.do", GET, "profile"));
-        blackList.add(new CommandRequestInfo("/product.do", GET, "add-product"));
-        BLACK_LIST = blackList;
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        ServletContext context = filterConfig.getServletContext();
+        InputStream is = context.getResourceAsStream("/authorization.properties");
+        
+        Properties properties = new Properties();
+        try {
+            properties.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        
+        this.blackList = properties.keySet().stream()
+                .map((e) -> (String) e)
+                .map((endpoint) -> CommandRequestInfo.of(endpoint.split(",")))
+                .collect(Collectors.toSet());
+        System.out.println("blackList=" + this.blackList);
     }
 
     @Override
@@ -33,7 +47,7 @@ public class AuthenticationFilter implements Filter {
     private void process(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws IOException, ServletException {
         if (request.getSession().getAttribute(CommonConstants.MEMBER_SESSION_KEY) != null
-                || !BLACK_LIST.contains(CommandRequestInfo.from(request))) {
+                || !this.blackList.contains(CommandRequestInfo.from(request))) {
             filterChain.doFilter(request, response);
             return;
         }
