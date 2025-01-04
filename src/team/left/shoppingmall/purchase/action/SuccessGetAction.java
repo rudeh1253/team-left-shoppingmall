ad5cb1e271c700319c216e297c2bd8e3733fa330
udongmandu <61107496+udongmandu@users.noreply.github.com>
2025-01-05 01:32:23 +0900

    css 수정 및 토스결제 api 추가

---
+++ src/team/left/shoppingmall/purchase/action/SuccessGetAction.java
@@ -0,0 +1,26 @@
+package team.left.shoppingmall.purchase.action;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import team.left.framework.web.CommandHandler;
+import team.left.shoppingmall.global.CommonConstants;
+import team.left.shoppingmall.global.PaginationTool;
+import team.left.shoppingmall.member.dao.MemberDao;
+import team.left.shoppingmall.purchase.dao.PurchaseDao;
+import team.left.shoppingmall.purchase.model.ReceiptDto;
+
+public class SuccessGetAction implements CommandHandler{
+	
+	@Override
+	public String handleCommand(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			
+		return "member/fragment/success";
+	}
+
+}
